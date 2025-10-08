05b5db7f943ddc474afcf5b867abe804673a5d02
Mehmet Ali Erfidan <erfidan740@gmail.com>
2025-10-08 18:24:43 +0300

    Disable dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="id">
+    <html lang="id" className="light">
       <body>
         {children}
         <Toaster position="top-right" />
